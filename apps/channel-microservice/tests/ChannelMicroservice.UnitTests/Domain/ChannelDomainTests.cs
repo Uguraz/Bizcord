10ce9330e54d23c38a52203d04e00db940aeaeb9
Uguraz <lassesyppli@yahoo.dk>
2025-10-19 14:30:18 +0200

    test(channel): domain tests for Channel invariants

---
+++ apps/channel-microservice/tests/ChannelMicroservice.UnitTests/Domain/ChannelDomainTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ChannelMicroservice.Domain.Channels;
+using FluentAssertions;
+using Xunit;
+
+namespace ChannelMicroservice.UnitTests.Domain;
+
+public class ChannelDomainTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Ctor_throws_when_name_is_missing(string? name)
+    {
+        var act = () => new Channel(name!);
+        act.Should().Throw<Exception>(); // ArgumentNullException eller ArgumentException
+    }
+
+    [Fact]
+    public void Ctor_throws_when_name_too_long()
+    {
+        var tooLong = new string('x', 101); // > 100
+        var act = () => new Channel(tooLong);
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("*1..100*");
+    }
+
+    [Fact]
+    public void Ctor_trims_and_sets_properties()
+    {
+        var ch = new Channel("  general  ");
+        ch.Name.Should().Be("general");
+        ch.Id.Should().NotBeNullOrWhiteSpace();
+        ch.CreatedAt.Should().BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(2));
+    }
+}
